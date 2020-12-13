bd806006c183e542d28a65c15df654b1c9f34465
RemiIia <valent.xarin@gmail.com>
2020-12-13 14:00:46 +0300

    Change hash size to 128

--- back-end/server/migrations/2020-11-03-034745_users/up.sql
+++ back-end/server/migrations/2020-11-03-034745_users/up.sql
@@ -2,7 +2,7 @@
 
 CREATE TABLE users (
   email VARCHAR(100) NOT NULL PRIMARY KEY,
-  hash VARCHAR(122) NOT NULL, --argon hash
+  hash VARCHAR(128) NOT NULL, --argon hash
   created_at TIMESTAMP NOT NULL
 );
 
