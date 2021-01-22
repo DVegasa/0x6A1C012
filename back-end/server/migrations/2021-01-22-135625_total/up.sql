decd682f60eab0c2e028c468dad2e1df785dab7a
RemiIia <valent.xarin@gmail.com>
2021-01-22 18:48:26 +0300

    Change some values

--- back-end/server/migrations/2021-01-22-135625_total/up.sql
+++ back-end/server/migrations/2021-01-22-135625_total/up.sql
@@ -1,70 +1,68 @@
 -- Your SQL goes here
 
 CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
-  login VARCHAR(32) NOT NULL,
-  name VARCHAR(32) NOT NULL,
-  surname VARCHAR(32) NOT NULL,
-  lastname VARCHAR(32) NOT NULL,
-  pswd_hash VARCHAR(128) NOT NULL,
-  role INTEGER,
-  created_at TIMESTAMP NOT NULL
+    id SERIAL PRIMARY KEY,
+    login VARCHAR(32) NOT NULL,
+    firstname VARCHAR(32) NOT NULL,
+    middlename VARCHAR(32) NOT NULL,
+    lastname VARCHAR(32) NOT NULL,
+    pswd_hash VARCHAR(128) NOT NULL,
+	role SMALLINT,
+    created_at TIMESTAMP NOT NULL
 );
 
-CREATE TABLE students (
-  student_id INTEGER REFERENCES users(id),
-  class_id INTEGER REFERENCES users(id)
-);
-
-CREATE TABLE teachers (
+CREATE TABLE subject ( 
+    id SERIAL PRIMARY KEY,
+    subject_name TEXT,
     teacher_id INTEGER REFERENCES users(id)
 );
 
-CREATE TABLE parents (
-    parent_id INTEGER REFERENCES users(id),
-    childrens_id INTEGER[] REFERENCES users(id)
+CREATE TABLE lesson (
+    id SERIAL PRIMARY KEY,
+    teacher_id INTEGER REFERENCES users(id),
+    meeting_room TEXT,
+    subject_id INTEGER REFERENCES subject(id),
+	slot SMALLINT,
+    lesson_date DATE NOT NULL
 );
 
 CREATE TABLE attendance (
-    subject_id INTEGER REFERENCES subject(id),
-    teacher_id INTEGER REFERENCES teachers(teacher_id),
-    student_id INTEGER REFERENCES students(student_id),
-    state BOOLEAN,
-    created_at TIMESTAMP NOT NULL
+    id SERIAL PRIMARY KEY,
+    lesson_id INTEGER REFERENCES lesson(id),
+    student_id INTEGER REFERENCES users(id),
+    state VARCHAR(2)
 );
 
 CREATE TABLE class (
-    teacher_id INTEGER REFERENCES teachers(teacher_id),
-    class_name VARCHAR(4),
-    student_id INTEGER[] REFERENCES users(id)
+    id SERIAL PRIMARY KEY,
+    classroom_teacher_id INTEGER,
+    year_of_study SMALLINT,
+    letter VARCHAR(1),
+    student_id INTEGER[] REFERENCES users(id),
+    FOREIGN KEY (classroom_teacher_id) REFERENCES users(id)
 );
 
 CREATE TABLE homework (
-    subject_id INTEGER REFERENCES subject(id),
-    teacher_id INTEGER REFERENCES teachers(teacher_id),
-    homework_text VARCHAR(255),
-    created_at TIMESTAMP NOT NULL
-);
-
-CREATE TABLE lesson (
-    teacher_id INTEGER REFERENCES teachers(teacher_id),
-    metting_room VARCHAR(32),
-    subject_name VARCHAR REFERENCES subject(subject_name)
+    id SERIAL PRIMARY KEY,
+    lesson_id INTEGER REFERENCES lesson(id),
+    homework_text TEXT,
+    deadline TIMESTAMP NOT NULL
 );
 
 CREATE TABLE mark (
-    subject_id INTEGER REFERENCES subject(id),
-    teacher_id INTEGER REFERENCES teachers(teacher_id),
-    student_id INTEGER REFERENCES students(student_id),
+    id SERIAL PRIMARY KEY,
+    lesson_id INTEGER REFERENCES lesson(id),
+    teacher_id INTEGER REFERENCES users(id),
+    student_id INTEGER REFERENCES users(id),
     mark VARCHAR(2),
-    coeffiecient VARCHAR(2),
-    created_at TIMESTAMP NOT NULL
+    coeffiecient REAL
 );
 
-create table observation (
-    subject_id INTEGER REFERENCES subject(id),
-    teacher_id INTEGER REFERENCES teachers(teacher_id),
-    student_id INTEGER REFERENCES students(student_id),
-    description TEXT,
-    created_at TIMESTAMP NOT NULL
+CREATE TABLE observation (
+    id SERIAL PRIMARY KEY,
+    lesson_id INTEGER REFERENCES lesson(id),
+    teacher_id INTEGER REFERENCES users(id),
+    student_id INTEGER REFERENCES users(id),
+    description TEXT
 );
+
