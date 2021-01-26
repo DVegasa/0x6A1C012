df52bba52a7e3c2fe7f0367388e4deff9c20ac62
RemiIia <valent.xarin@gmail.com>
2021-01-26 20:43:20 +0300

    Fix derive macro error

--- back-end/server/src/models.rs
+++ back-end/server/src/models.rs
@@ -1,12 +1,12 @@
-use super::schema::*;
+// use super::schema::*;
 use diesel::{r2d2::ConnectionManager, PgConnection};
 use serde::{Deserialize, Serialize};
 
 // type alias to use in multiple places
 pub type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
 
-#[derive(Debug, Serialize, Deserialize, Queryable, Insertable)]
-#[table_name = "users"]
+#[derive(Debug, Serialize, Deserialize, Queryable)]
+#[diesel(table_name = "users")]
 pub struct User {
     pub id: i32,
     pub login: String,
@@ -18,27 +18,16 @@ pub struct User {
     pub created_at: chrono::NaiveDateTime,
 }
 
-#[derive(Debug, Serialize, Deserialize, Queryable, Insertable)]
-#[table_name = "subject"]
+#[derive(Debug, Serialize, Deserialize, Queryable)]
+#[diesel(table_name = "subject")]
 pub struct Subject {
     pub id: i32,
     pub subject_name: String,
     pub teacher_id: i32,
 }
 
-#[derive(Debug, Serialize, Deserialize, Queryable, Insertable)]
-#[table_name = "lesson"]
-pub struct Lesson {
-    pub id: i32,
-    pub teacher_id: i32,
-    pub meeting_room: String,
-    pub subject_id: i32,
-    pub slot: i16,
-    pub lesson_date: chrono::NaiveDateTime,
-}
-
-#[derive(Debug, Serialize, Deserialize, Queryable, Insertable)]
-#[table_name = "attendance"]
+#[derive(Debug, Serialize, Deserialize, Queryable)]
+#[diesel(table_name = "attendance")]
 pub struct Attendance {
     pub id: i32,
     pub lesson_id: i32,
@@ -46,8 +35,8 @@ pub struct Attendance {
     pub state: String,
 }
 
-#[derive(Debug, Serialize, Deserialize, Queryable, Insertable)]
-#[table_name = "class"]
+#[derive(Debug, Serialize, Deserialize, Queryable)]
+#[diesel(table_name = "class")]
 pub struct Class {
     pub id: i32,
     pub classroom_teacher_id: i32,
@@ -55,16 +44,16 @@ pub struct Class {
     pub letter: String,
 }
 
-#[derive(Debug, Serialize, Deserialize, Queryable, Insertable)]
-#[table_name = "class_student"]
+#[derive(Debug, Serialize, Deserialize, Queryable)]
+#[diesel(table_name = "class_student")]
 pub struct ClassStudent {
     pub id: i32,
     pub student_id: i32,
     pub class_id: i32,
 }
 
-#[derive(Debug, Serialize, Deserialize, Queryable, Insertable)]
-#[table_name = "homework"]
+#[derive(Debug, Serialize, Deserialize, Queryable)]
+#[diesel(table_name = "homework")]
 pub struct Homework {
     pub id: i32,
     pub lesson_id: i32,
@@ -72,8 +61,8 @@ pub struct Homework {
     pub deadline: chrono::NaiveDateTime,
 }
 
-#[derive(Debug, Serialize, Deserialize, Queryable, Insertable)]
-#[table_name = "mark"]
+#[derive(Debug, Serialize, Deserialize, Queryable)]
+#[diesel(table_name = "mark")]
 pub struct Mark {
     pub id: i32,
     pub lesson_id: i32,
@@ -83,8 +72,8 @@ pub struct Mark {
     pub coeffiecient: f32,
 }
 
-#[derive(Debug, Serialize, Deserialize, Queryable, Insertable)]
-#[table_name = "observation"]
+#[derive(Debug, Serialize, Deserialize, Queryable)]
+#[diesel(table_name = "observation")]
 pub struct Observation {
     pub id: i32,
     pub lesson_id: i32,
@@ -93,15 +82,27 @@ pub struct Observation {
     pub description: String,
 }
 
-impl User {
-    pub fn from_details<S: Into<String>, T: Into<String>>(login: S, pwd: T) -> Self {
-        User {
-            login: login.into(),
-            pswd_hash: pwd.into(),
-            created_at: chrono::Local::now().naive_local(),
-        }
-    }
+// TODO: Insertable version of this struct
+#[derive(Debug, Serialize, Deserialize, Queryable)]
+#[diesel(table_name = "lesson")]
+pub struct Lesson {
+    pub id: i32,
+    pub teacher_id: i32,
+    pub meeting_room: String,
+    pub subject_id: i32,
+    pub slot: i16,
+    pub lesson_date: chrono::NaiveDateTime,
 }
+
+// impl User {
+//     pub fn from_details<S: Into<String>, T: Into<String>>(login: S, pwd: T) -> Self {
+//         User {
+//             login: login.into(),
+//             pswd_hash: pwd.into(),
+//             created_at: chrono::Local::now().naive_local(),
+//         }
+//     }
+// }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct SlimUser {
